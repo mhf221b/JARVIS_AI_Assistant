e103d20b55a5bf02b52bb7e188273a8cec86720d
Mehedi Hasan <mhf221b@gmail.com>
2022-02-16 06:06:17 +0600

    Added Date Function and a simple Greeting

--- JARVIS/JARVIS.py
+++ JARVIS/JARVIS.py
@@ -6,7 +6,7 @@ import datetime
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-newVoiceRate = 160 #I am setting the speed of the voice, 140 means 140 words per minute
+newVoiceRate = 175 #I am setting the speed of the voice, 140 means 140 words per minute
 engine.setProperty('rate', newVoiceRate)
 
 def speak(audio): #this function is used for converting text to speech
@@ -15,8 +15,38 @@ def speak(audio): #this function is used for converting text to speech
 
 def time(): #This Function can say what time it is
     time = datetime.datetime.now().strftime("%H:%M:%S")
+    speak("The time at the moment is:")
     speak(time)
 
-time()
+def date(): #This Function can say what date it is
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    day = datetime.datetime.now().day
+    speak("The date today is:")
+    speak(day)
+    speak(month)
+    speak(year)
 
-speak("Hello World! I am JARVIS and I am here to serve Mehedi.")
+def wishMe(): #This wish will execuse every time I start up this software
+    hour = datetime.datetime.now().hour
+    print(hour)
+    if(hour >= 6 and hour <= 12):
+        speak("Good Morning, Sir!")
+    elif(hour>12 and hour<=18 ):
+        speak("Good Afternoon, Sir!")
+    elif(hour>18 and hour<=23):
+        speak("Good Evening, Sir!")
+    else:
+        speak("Good Night Sir!")
+    speak("Hello! JARVIS at your service")
+    time()
+    date()
+    speak("What can I assist you with?")
+
+wishMe()
+
+# time()
+# date()
+
+# speak("Hello World! I am JARVIS and I am here to serve Mehedy.")
+
