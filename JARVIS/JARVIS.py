9f09f7a89e816862dd2294045dd48085c2fa6670
Mehedi Hasan <mhf221b@gmail.com>
2022-04-20 06:49:35 +0600

    Version 1.0
    Big Update

--- JARVIS/JARVIS.py
+++ JARVIS/JARVIS.py
@@ -1,4 +1,5 @@
 from socket import timeout
+from socket import *
 from sqlite3 import Date
 from urllib import request
 import pyttsx3
@@ -9,13 +10,20 @@ import wikipedia #used the command pip install wikipedia to get this module
 import webbrowser as wb #For opening browser
 import os
 import random
-import tkinter as tk
 import requests
 import pyautogui
 import win32api
 import sys
-from PyQt5 import QtGui
-from PyQt5.QtCore import QTimer, QTime, QDate
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import QTimer, QTime, QDate, Qt
+from PyQt5.QtGui import QMovie
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.uic import loadUiType
+from JarvisGUI import Ui_JarvisFrontEnd
+
+
 
 
 engine = pyttsx3.init()
@@ -45,6 +53,7 @@ timeout = 5
     # print("All systems offline")
     # exit()
 
+
 def time(): #This Function can say what time it is
     time = datetime.datetime.now().strftime("%H:%M:%S")
     speak("The time at the moment is:")
@@ -73,86 +82,122 @@ def wishMe(): #This wish will execuse every time I start up this software
     speak("What can I assist you with?")
     speak("I am Listening")
 
-def takeCommand():  #This is the function which recognizes my speech
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        r.adjust_for_ambient_noise(source)
-        #r.pause_threshold = 1
-        audio = r.listen(source)
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en=US')
-        speak(query)
-    except Exception as e:
-        print(e)
-        speak("Sorry, I can't hear you")
-        return "None"
-    return query
-
-# screen_main = tk.Tk()
-# screen_main.configure(background='black') #Color of the main background window
-# screen_main.attributes('-fullscreen', True) #Window should be full screen
-
-
-# def color_changer():
-#     color = ['red', 'gold', 'silver', 'cyan', 'magenta']
-#     actual_color = random.choice(color)
-#     label.configure(foreground=actual_color)
-#     label.after(500, color_changer)
-
-# label = Label(screen_main, font = ('Arial', 35), text="Welcome to Avengers", background = 'black')
-# color_changer()
-# label.place(x=1060, y=50)
-# screen_main.mainloop()
-
-# if __name__ =="__main__": #Implementing the main function
-#     clear = lambda: os.system('cls')
-#     clear()
-#     wishMe()
+class MainThread(QThread):
+    def __init__(self):
+        super(MainThread, self).__init__()
     
-#     while True:
-#         query = takeCommand().lower()
-#         print(query)
+    def run(self):
+        self.taskExecution()
 
-#         if "time" in query:
-#             time()
-#         elif "date" in query:
-#             date()
-#         elif "offline" in query:
-#             speak("Going Offline")
-#             quit()
-#         elif "wikipedia" in query:
-#             speak("Searching")
-#             query = query.replace("wikipedia", "")
-#             result = wikipedia.summary (query, sentences = 2)
-#             speak(result)
-#         elif "how are you" in query:
-#             speak("I am fine, Thank you!")
-#             speak("What about you")
-#         elif ("good" or "fine") in query:
-#             speak("I'm glad to hear that")
+    def takeCommand(self):  #This is the function which recognizes my speech
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listening...")
+            r.adjust_for_ambient_noise(source)
+            #r.pause_threshold = 1
+            audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language='en=US')
+            # speak(query)
+        except Exception as e:
+            print(e)
+            speak("Sorry, I can't hear you")
+            return "None"
+        return query
 
-#         elif "open chrome" in query:
-#             speak("Where should I go?")
-#             chromepath = "C:/Users/DOLPHIN/AppData/Local/Google/Chrome/Application/chrome.exe %s"
-            
-#             search = takeCommand().lower()
-#             wb.get(chromepath).open_new_tab(search + ".com")
-#         elif "search in chrome" in query:
-#             chromepath = "C:/Users/DOLPHIN/AppData/Local/Google/Chrome/Application/chrome.exe %s"
-#             speak("What should I search for?")
-#             query = takeCommand().lower()
-#             query = query.replace("search for", "")
-#             speak("Searching for"+query+"in chrome")
-#             wb.get(chromepath).open("https://www.google.com/search?q="+query)
-            
 
-#         elif "play a song" in query:
-#             songs_dir = "C:/Users/DOLPHIN/Downloads/Music %s"
-#             songs = os.listdir(songs_dir)
-#             n = random(0, 107)
-#             os.startfile(os.path.join(songs_dir, songs[n] ))
+    # if __name__ =="__main__": #Implementing the main function
+    def taskExecution(self):
+        clear = lambda: os.system('cls')
+        clear()
+        wishMe()
+        
+        while True:
+            self.query = self.takeCommand().lower()
+            print(self.query)
+
+            if "time" in self.query:
+                time()
+            elif "date" in self.query:
+                date()
+            elif "offline" in self.query:
+                speak("Going Offline")
+                quit()
+            elif "wikipedia" in self.query:
+                speak("Searching")
+                query = query.replace("wikipedia", "")
+                result = wikipedia.summary (query, sentences = 2)
+                speak(result)
+            elif "how are you" in self.query:
+                speak("I am fine, Thank you!")
+                speak("What about you")
+            elif "good" in self.query:
+                speak("I'm glad to hear that.")
+                speak("what do you want me to do?")
+
+            elif "fine" in self.query:
+                speak("I'm glad to hear that.")
+                speak("what do you want me to do?")
+
+
+            elif "open chrome" in self.query:
+                speak("Where should I go?")
+                chromepath = "C:/Users/DOLPHIN/AppData/Local/Google/Chrome/Application/chrome.exe %s"
+                
+                search = self.takeCommand().lower()
+                wb.get(chromepath).open_new_tab(search + ".com")
+            elif "search in chrome" in self.query:
+                chromepath = "C:/Users/DOLPHIN/AppData/Local/Google/Chrome/Application/chrome.exe %s"
+                speak("What should I search for?")
+                query = self.takeCommand().lower()
+                query = query.replace("search for", "")
+                speak("Searching for"+query+"in chrome")
+                wb.get(chromepath).open("https://www.google.com/search?q="+query)
+                
+
+            elif "play a song" in self.query:
+                songs_dir = "C:/Users/DOLPHIN/Downloads/Music %s"
+                songs = os.listdir(songs_dir)
+                n = random(0, 107)
+                os.startfile(os.path.join(songs_dir, songs[n] ))
+
+startExecution = MainThread()
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_JarvisFrontEnd()
+        self.ui.setupUi(self)
+        self.ui.Run.clicked.connect(self.startTask)
+        self.ui.Run_2.clicked.connect(self.close)
+
+    def startTask(self):
+        self.ui.movie = QtGui.QMovie("E:\Books\CSE 410\JARVIS AI Assistant\JARVIS_AI_Assistant\Iron Dad and Spider Son One-Shots.gif")
+        self.ui.label.setMovie(self.ui.movie)
+        self.ui.movie.start()
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.showTime)
+        timer.start(1000)
+
+        startExecution.start()
+
+    def showTime(self):
+        current_time = QTime.currentTime()
+        current_date = QDate.currentDate()
+        Label_time = current_time.toString('hh: mm: pp')
+        Label_date = current_date.toString(Qt.ISODate) 
+
+        self.ui.textBrowser.setText("Date:"+Label_date)
+        self.ui.textBrowser_2.setText("Time:"+Label_time)
+
+app = QApplication(sys.argv)
+Jarvis = Main()
+Jarvis.show()
+exit(app.exec_())
+
+
 
 
 
@@ -164,4 +209,3 @@ def takeCommand():  #This is the function which recognizes my speech
 # date()
 
 # speak("Hello World! I am JARVIS and I am here to serve Mehedy.")
-
